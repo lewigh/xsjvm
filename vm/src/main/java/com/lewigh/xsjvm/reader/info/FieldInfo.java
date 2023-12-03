9493cd9799eafa3abe1471a620ae51346a91878f
Rob B <lewighrb@gmail.com>
2023-12-04 01:29:11 +0300

    eq hsh tstr

--- vm/src/main/java/com/lewigh/xsjvm/reader/info/FieldInfo.java
+++ vm/src/main/java/com/lewigh/xsjvm/reader/info/FieldInfo.java
@@ -4,6 +4,9 @@ package com.lewigh.xsjvm.reader.info;
 import com.lewigh.xsjvm.reader.flag.FiledAccessFlag;
 import com.lewigh.xsjvm.reader.info.attribute.AttributeInfo;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record FieldInfo(short accessFlags,
                         String name,
                         String descriptor,
@@ -49,5 +52,30 @@ public record FieldInfo(short accessFlags,
     private boolean hasFlag(FiledAccessFlag flag) {
         int hex = flag.getHex();
         return (accessFlags & hex) == hex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FieldInfo fieldInfo = (FieldInfo) o;
+        return accessFlags == fieldInfo.accessFlags && Objects.equals(name, fieldInfo.name) && Objects.equals(descriptor, fieldInfo.descriptor) && Arrays.equals(attributes, fieldInfo.attributes);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(accessFlags, name, descriptor);
+        result = 31 * result + Arrays.hashCode(attributes);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "FieldInfo{" +
+                "accessFlags=" + accessFlags +
+                ", name='" + name + '\'' +
+                ", descriptor='" + descriptor + '\'' +
+                ", attributes=" + Arrays.toString(attributes) +
+                '}';
     }
 }
